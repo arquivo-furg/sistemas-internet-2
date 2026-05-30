ce8f27c5d12cf96ea9c56ab15a45c801f333874b
pedromchd <48498905+pedromchd@users.noreply.github.com>
2026-05-29 22:39:11 -0300

    remove: handling for common favicon and certificate errors in proxy function

--- proxy/app.py
+++ proxy/app.py
@@ -134,8 +134,6 @@ def proxy(url):
             # allow_redirects=False,
         )
     except Exception:
-        if dominio in ["favicon.ico", ".well-known"]:
-            return ("", 204)  # Ignora erros comuns de favicon e certificados
         registrar_log(dominio, ERRO)
         return (render_template("erro.html", dominio=dominio, codigo=502), 502)
 
